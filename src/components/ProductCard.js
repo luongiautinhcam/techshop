7280626bbc5afaf09c9df84b62cfcb70af547461
Vinh <tranthanhvinh.info@gmail.com>
2023-03-30 00:53:24 +0700

    OurStore: products-list

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,55 +1,119 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  const { grid } = props;
+  let location = useLocation();
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
-        <div className="wishlist-icon position-absolute">
-          <Link>
-            <img src="images/wish.svg" alt="wishlist" />
-          </Link>
-        </div>
-        <div className="product-image">
-          <img
-            src="images/watch.jpg"
-            className="img-fluid"
-            alt="product image"
-          />
-          <img
-            src="images/tab.jpg"
-            className="img-fluid"
-            alt="product image"
-          />
-        </div>
-        <div className="product-details">
-          <h6 className="brand">Havels</h6>
-          <h5 className="product-title">Tai nghe cho học sinh tiểu học</h5>
-          <ReactStars
-            count={5}
-            size={24}
-            value={3}
-            edit={false}
-            activeColor="#ffd700"
-          />
-          <p className="price">200.000đ</p>
-        </div>
-        <div className="action-bar position-absolute">
-          <div className="d-flex flex-column gap-15">
+    <>
+      <div
+        className={` ${location.pathname == "/store" ? `gr-${grid}` : "col-3"}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
             <Link>
-              <img src="images/prodcompare.svg" alt="compare" />
-            </Link>
-            <Link>
-              <img src="images/view.svg" alt="view" />
-            </Link>
-            <Link>
-              <img src="images/add-cart.svg" alt="addcart" />
+              <img src="images/wish.svg" alt="wishlist" />
             </Link>
           </div>
-        </div>
-      </Link>
-    </div>
+          <div className="product-image">
+            <img
+              src="images/watch.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+            <img
+              src="images/tab.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">Tai nghe cho học sinh tiểu học</h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={3}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              Khi chiếc xe máy vừa dừng lại, người mặc trang phục giống dân quân
+              tự vệ liền vụt gậy vào người nam sinh chạy xe máy, kéo nam sinh
+              xuống xe, tì chân lên người để khống chế.
+            </p>
+            <p className="price">200.000đ</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={` ${location.pathname == "/store" ? `gr-${grid}` : "col-3"}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wishlist" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img
+              src="images/watch.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+            <img
+              src="images/tab.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">Tai nghe cho học sinh tiểu học</h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={3}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              Khi chiếc xe máy vừa dừng lại, người mặc trang phục giống dân quân
+              tự vệ liền vụt gậy vào người nam sinh chạy xe máy, kéo nam sinh
+              xuống xe, tì chân lên người để khống chế.
+            </p>
+            <p className="price">200.000đ</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactStars from "react-rating-stars-component";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ProductCard from "../components/ProductCard";
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
   return (
     <>
       <Meta title={"Cửa hàng chúng tôi"} />
@@ -181,7 +183,75 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex algin-items-center gap-10">
+                    <p className="mb-0 d-block" style={{ width: "100px" }}>
+                      Sắp xếp theo:
+                    </p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual">Đặc tính</option>
+                      <option value="best-selling" selected="selected">
+                        Bán chạy
+                      </option>
+                      <option value="title-ascending">A-Z</option>
+                      <option value="title-descending">Z-A</option>
+                      <option value="price-ascending">Giá, thấp đến cao</option>
+                      <option value="price-descending">
+                        Giá, cao đến thấp
+                      </option>
+                      <option value="create-ascending">Ngày, cũ đến mới</option>
+                      <option value="create-descending">
+                        Ngày, mới đến cũ
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Sản phẩm</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGrid(3);
+                        }}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <div className="d-flex gap-10 flex-wrap">
+                  <ProductCard grid={grid} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
